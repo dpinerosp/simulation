6f5ba0ac2f7be4600a1887d30a257e639aa53ab2
Danilo Piñeros <dspinerosp@gmail.com>
2021-03-04 15:00:10 -0500

    Create directory mm1
    The directory mm1 has the files parameters.py, routines.py and mm1.py.
    
    parameters.py will have the parameter values needed to the simulation of a systems M/M/1.
    routines.py is intended to have the routines initialization, timing, library, report_generator,
    arrival_event, departure_event and expond.
    mm1.py will have the general logic of the simulation.

---
+++ mm1/mm1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import parameters
+import routines
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
---
+++ mm1/routines.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+def initialization():
+    pass
+
+def timing():
+    pass
+
+def library():
+    pass
+
+def report_generator():
+    pass
+
+def arrival_evennt():
+    pass
+
+def departure_event():
+    pass
+
+def expon():
+    pass
